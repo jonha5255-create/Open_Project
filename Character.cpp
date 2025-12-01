f3926881e0e7e02ecceec708604f370cbd6c918e
KIMjunayo <wnsgk5255@tukorea.ac.kr>
2025-12-01 20:52:05 +0900

    추가

--- Character.cpp
+++ Character.cpp
@@ -1,1 +1,14 @@
 // glew32.lib freeglut.lib 넣었을거임
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <time.h> //랜덤함수
+#include <algorithm>
+#include <vector>
+
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
